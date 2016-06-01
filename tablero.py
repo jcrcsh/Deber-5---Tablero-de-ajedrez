894a5313c7988eec6aed37bc186878e1a2a81bc6
José Ruiz <jcrcsh@gmail.com>
2016-06-01 16:40:16 -0500

    version 1.1

--- tablero.py
+++ tablero.py
@@ -5,3 +5,5 @@ ventana.title("Tablero de ajedrez")
 for i in range(7): //0, 1, 2, 3, 4, 5, 6, 7
     for j in range(7):
         b = Button(ventana, text = "A 1", heigh = 10, width = 10).place(x = i * 20, y = j * 20)
+
+este codigo corre bien en java
