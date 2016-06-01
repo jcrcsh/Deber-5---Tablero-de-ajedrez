327da13b3ee824630e42a6c0fe21879894f907b3
José Ruiz <jcrcsh@gmail.com>
2016-06-01 16:30:16 -0500

    version 1.0

---
+++ tablero.py
@@ -0,0 +1,6 @@
+from tkinter import *
+ventana = Tk()
+ventana.geometry("1000x1000+100+100")
+ventana.title("Tablero de ajedrez")
+//aqui va el for
+bA1 = Button(ventana, text = "A 1", heigh = 10, width = 10).place(x = 50, y = 20)
