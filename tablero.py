a6038cb59c1f10e2de46075d453db74f95c58a8a
José Ruiz <jcrcsh@gmail.com>
2016-06-01 16:39:12 -0500

    version 1.1

--- tablero.py
+++ tablero.py
@@ -2,5 +2,6 @@ from tkinter import *
 ventana = Tk()
 ventana.geometry("1000x1000+100+100")
 ventana.title("Tablero de ajedrez")
-//aqui va el for
-bA1 = Button(ventana, text = "A 1", heigh = 10, width = 10).place(x = 50, y = 20)
+for i in range(7): //0, 1, 2, 3, 4, 5, 6, 7
+    for j in range(7):
+        b = Button(ventana, text = "A 1", heigh = 10, width = 10).place(x = i * 20, y = j * 20)
